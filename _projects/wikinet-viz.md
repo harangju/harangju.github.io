cfc74b69832df8ae9115a2890b7c690b40f626e1
Harang Ju <harangju@gmail.com>
2020-10-25 14:17:02 -0400

    network viz tutorial

--- _projects/wikinet-viz.md
+++ _projects/wikinet-viz.md
@@ -7,33 +7,10 @@ importance: 1
 ---
 
 <script type="text/javascript" src="https://d3js.org/d3.v3.min.js"></script>
-<script type="text/javascript" src="/assets/js/wikinet_viz.js"></script>
+<script defer type="text/javascript" src="/assets/js/wikinet_viz.js"></script>
 
 In my recent <a href='http://arxiv.org/abs/2010.08381' target='blank'>paper</a>,
- we use persistent homology to identify knowledge gaps. Here is a visualization
- of knowledge gaps in Wikipedia networks.
+ we use persistent homology to identify knowledge gaps. Explore knowledge gaps
+ in networks of hyperlinked science articles on Wikipedia.
 
 <div class="viz"></div>
-
-<script type="text/javascript">
-
-var sampleSVG = d3.select('.viz')
-        .append('svg')
-        .attr({width: 600, height: 100});
-
-var data = d3.range(5).map(function(d, i){ return ~~(Math.random()*100); })
-
-sampleSVG.selectAll('circle')
-        .data(data)
-        .enter().append('circle')
-        .style({stroke: 'gray', fill: 'aliceblue'})
-        .attr({r: 40, cx: function(d, i){ return i*100 + 50; }, cy: 50})
-        .call(d3.helper.tooltip()
-            .attr({class: function(d, i) { return d + ' ' +  i + ' A'; }})
-            .style({color: 'blue'})
-            .text(function(d, i){ return 'value: '+d; })
-        )
-        .on('mouseover', function(d, i){ d3.select(this).style({fill: 'skyblue'}); })
-        .on('mouseout', function(d, i){ d3.select(this).style({fill: 'aliceblue'}); });
-
-</script>
--- assets/js/wikinet_viz.js
+++ assets/js/wikinet_viz.js
@@ -1,4 +1,5 @@
 // http://www.biovisualize.com/2012/07/embedding-d3js-in-blog-post.html
+'use strict'
 
 d3.helper = {};
 
@@ -67,3 +68,80 @@ d3.helper.tooltip = function(){
 
     return tooltip;
 };
+
+let drag = simulation => {
+
+  function dragstarted(event, d) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(event,d) {
+    d.fx = event.x;
+    d.fy = event.y;
+  }
+
+  function dragended(event,d) {
+    if (!event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
+  return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+}
+
+const height = 680
+
+var json = null;
+function update() {
+  const nodes = json.links.map(d => Object.create(d));
+  const links = json.links.map(d => Object.create(d));
+  console.log(nodes);
+
+  function tick() {
+    viz_nodes.attr('cx', function(d) { return d.x; })
+      .attr('cy', function(d) { return d.y; });
+    viz_links.attr('x1', function(d) { return d.source.x })
+      .attr('y1', function(d) { return d.source.y })
+      .attr('x2', function(d) { return d.target.x })
+      .attr('y2', function(d) { return d.target.y });
+  }
+
+  let force = d3.layout.force()
+    .nodes(nodes)
+    .size([600, 600])
+    .gravity(.3)
+    // .links(links)
+    .linkDistance(80)
+    .charge(-100)
+    .on('tick', tick);
+  force.start();
+
+  let network = d3
+    .select('.viz')
+    .append('svg')
+    .attr({width: 600, height: 600});
+
+  let viz_nodes = network.selectAll("circle")
+    .data(force.nodes())
+    .enter().append("circle")
+    .style("fill", "steelblue")
+    .attr("r", 4);
+
+  let viz_links = network.selectAll("lines")
+    .data(force.links())
+    .enter().append("line")
+}
+
+$.getJSON("/assets/graph.json", function(data) {
+  console.log('graph file');
+  json = data;
+  console.log(json);
+  console.log(json['nodes'][0]);
+  console.log(json['links'][0]);
+  update();
+});
