eb5aeb2037603c7a5dd99e532264c7d61bfcce25
Harang Ju <harangju@gmail.com>
2020-10-26 01:40:16 -0400

    tooltip

--- assets/js/wikinet_viz.js
+++ assets/js/wikinet_viz.js
@@ -1,10 +1,22 @@
 // http://www.biovisualize.com/2012/07/embedding-d3js-in-blog-post.html
+// https://github.com/vlandham/force_talk/blob/gh-pages/slides/simple_force.html
 'use strict'
 
 let svg = d3
   .select('.viz')
   .append('svg')
   .attr({width: 600, height: 600});
+
+// https://www.d3-graph-gallery.com/graph/interactivity_tooltip.html
+let tooltip = d3.select(".viz")
+  .insert("div")
+  .attr("class", "tooltip")
+  .style('opacity', 0)
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "2px")
+  .style("border-radius", "5px")
+  .style("padding", "5px");
 
 let force = d3.layout.force()
   .on("tick", tick)
@@ -34,28 +46,39 @@ function update() {
   force.nodes(nodes)
     .links(links);
 
-  node = svg.selectAll("circle")
-    .data(nodes)
-    .enter().append("circle")
-    .style("fill", "steelblue")
-    .attr("r", d => 4.5)
-    .call(force.drag);
   link = svg.selectAll("line")
     .data(links)
     .enter().append("line")
     .attr("class", "link")
     .style("stroke-width", 1)
-    .style("stroke", "#3182bd");
+    .style("stroke", "steelblue");
+  node = svg.selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+    .style("fill", "steelblue")
+    .attr("r", d => 4.5)
+    .call(force.drag)
+    .on('mouseover', function(d) {
+      tooltip.html(d.id)
+        .style('opacity', 1);
+    })
+    .on('mousemove', function(d) {
+      tooltip.style('left', (d3.mouse(this)[0]) + 'px')
+        .style('top', (d3.mouse(this)[1]+300) + 'px');
+    })
+    .on('mouseout', function(d) {
+      tooltip.style('opacity', 0);
+    });
 
   force.start();
 }
 
 var node, link;
 function tick() {
-  node.attr('cx', d => d.x)
-    .attr('cy', d => d.y);
   link.attr('x1', d => d.source.x)
     .attr('y1', d => d.source.y)
     .attr('x2', d => d.target.x)
     .attr('y2', d => d.target.y);
+  node.attr('cx', d => d.x)
+    .attr('cy', d => d.y);
 }
